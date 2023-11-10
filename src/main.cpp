bbf2c3967aeef4a9af6ba987821b774d49093909
Martin Fiedler <keyj@emphy.de>
2023-11-10 22:46:29 +0100

    initial empty shell
    It's little more than an SDL+OpenGL hello-world application by now,
    but it's a start.
    
    The CMakeLists boilerplate has been lifted from GIPS almost completely.

---
+++ src/main.cpp
@@ -0,0 +1,99 @@
+#ifdef _WIN32
+    #define WIN32_LEAN_AND_MEAN
+    #define NOMINMAX
+    #include <windows.h>
+#endif
+
+#include <cstdio>
+#include <cstdlib>
+
+#include <SDL.h>
+#include <glad/glad.h>
+
+[[noreturn]] static void fatal(const char *what, const char *how) {
+    #if defined(_WIN32) && defined(NDEBUG)
+        MessageBoxA(nullptr, how, what, MB_OK | MB_ICONERROR);
+    #else
+        fprintf(stderr, "FATAL: %s - %s\n", what, how);
+    #endif
+    std::exit(1);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[]) {
+    (void) argc, (void) argv;
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+        fatal("SDL initialization failed", SDL_GetError());
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,            0);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE,          0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,  SDL_GL_CONTEXT_PROFILE_CORE);
+    #ifdef _DEBUG
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,     SDL_GL_CONTEXT_DEBUG_FLAG);
+    #endif
+
+    SDL_Window *win = SDL_CreateWindow(
+        "Tracked Music Compo Player",
+        #ifdef _DEBUG
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0
+        #else
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_FULLSCREEN_DESKTOP
+        #endif
+        | SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
+    if (!win) {
+        fatal("could not create window", SDL_GetError());
+    }
+    #ifdef NDEBUG
+        SDL_ShowCursor(SDL_DISABLE);
+    #endif
+
+    SDL_GLContext ctx = SDL_GL_CreateContext(win);
+    if (!ctx) {
+        fatal("could not create OpenGL context", SDL_GetError());
+    }
+    SDL_GL_MakeCurrent(win, ctx);
+    SDL_GL_SetSwapInterval(1);
+    if (!gladLoadGL()) {
+        fatal("could not initialize OpenGL", "failed to load OpenGL functions");
+    }
+    #ifdef _DEBUG
+        printf("OpenGL vendor:   %s\n", glGetString(GL_VENDOR));
+        printf("OpenGL renderer: %s\n", glGetString(GL_RENDERER));
+        printf("OpenGL version:  %s\n", glGetString(GL_VERSION));
+        printf("GLSL   version:  %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+    #endif
+
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+
+    bool active = true;
+    while (active) {
+        SDL_Event ev;
+        while (SDL_PollEvent(&ev)) {
+            switch (ev.type) {
+                case SDL_KEYDOWN:
+                    if (ev.key.keysym.sym == SDLK_q) { active = false; }
+                    break;
+                case SDL_QUIT:
+                    active = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(win);
+    }
+
+    SDL_GL_MakeCurrent(nullptr, nullptr);
+    SDL_GL_DeleteContext(ctx);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
