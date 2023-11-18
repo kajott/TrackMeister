kajott/TrackMeister

commit 3c73974a04cfe0bb56fbf47a32de1b8581c2fa4d

implement pattern display

src/app_layout.cpp
@@ -1,9 +1,12 @@
 // SPDX-FileCopyrightText: 2023 Martin J. Fiedler <keyj@emphy.de>
 // SPDX-License-Identifier: MIT
 
+#include <cstring>
 #include <cmath>
+
 #include <algorithm>
 
+#include "system.h"
 #include "config.h"
 #include "renderer.h"
 #include "app.h"
@@ -20,8 +23,10 @@ void Application::updateLayout() {
     m_screenSizeX = m_renderer.viewportWidth();
     m_screenSizeY = m_renderer.viewportHeight();
 
+    // set up "no module loaded" screen geometry
     m_emptyTextSize = toPixels(m_config.emptyTextSize);
 
+    // set up info box geometry
     if (m_filename.empty() && m_title.empty() && m_artist.empty() && m_details.empty()) {
         // no info box at all
         m_infoEndY = m_infoShadowEndY = 0;
@@ -58,4 +63,89 @@ void Application::updateLayout() {
         m_infoEndY += lineSpacing + toPixels(m_config.infoMarginY);
         m_infoShadowEndY = m_infoEndY + m_config.infoShadowSize;
     }
+
+    // set up metadata box geometry
+    m_metaStartX = m_screenSizeX;
+    m_metaShadowStartX = m_metaStartX - 0;
+
+    // set up pattern display geometry -- step 1: define possible formats
+    struct PDFormat { const char *posFormat, *channelFormat, *sep; };
+    static const PDFormat pdFormats[] = {
+        { "",            "G#0",           "W"  },
+        { "000",         "G#0",           "W"  },
+        { "000",         "G#0 00",        "W"  },
+        { "000",         "G#0 00",        "WW" },
+        { "000:000",     "G#0 00",        "W"  },
+        { "000:000",     "G#0 00",        "WW" },
+        { "000:000",     "G#0 00v00",     "W"  },
+        { "000:000",     "G#0 00v00",     "WW" },
+        { "000:000.000", "G#0 00v00",     "W"  },
+        { "000:000.000", "G#0 00v00",     "WW" },
+        { "000:000.000", "G#0 00v00 C00", "W"  },
+        { "000:000.000", "G#0 00v00 C00", "WW" },
+        { nullptr, nullptr, nullptr }
+    };
+
+    // set up pattern display geometry -- step 2: the actual formatting function
+    auto doFormat = [&] (const PDFormat &fmt) -> int {
+        m_pdPosChars = strlen(fmt.posFormat);
+        m_pdChannelChars = strlen(fmt.channelFormat);
+        int gapWidth = textWidth(m_pdTextSize, fmt.sep);
+        m_pdTextY0 = (m_infoEndY + m_screenSizeY - m_pdTextSize) >> 1;
+        m_pdTextDY = m_pdTextSize + toPixels(m_config.patternLineSpacing);
+        m_pdRows = (m_pdTextY0 - m_infoEndY + m_pdTextSize - 1) / m_pdTextSize;
+        m_pdChannelX0 = m_pdPosChars ? (textWidth(m_pdTextSize, fmt.posFormat) + gapWidth) : 0;
+        m_pdChannelDX = textWidth(m_pdTextSize, fmt.channelFormat) + gapWidth;
+        m_pdPipeDX = 0.5f * float(m_pdTextSize) * (m_renderer.textWidth(fmt.sep) + m_renderer.textWidth("|"));
+        m_pdBarRadius = (m_pdTextSize * m_config.patternBarBorderPercent) / 100;
+        return m_pdChannelX0 + m_numChannels * m_pdChannelDX - gapWidth;
+    };
+
+    // set up pattern display geometry -- step 3: try various formats to fit width
+    m_pdTextSize = toPixels(m_config.patternTextSize);
+    int pdMaxWidth = m_metaStartX - 2 * toPixels(m_config.patternMarginX);
+    // try most compact format first
+    int pdWidth = doFormat(pdFormats[0]);
+    if (pdWidth > pdMaxWidth) {
+        // if the most compact format doesn't fit, we need to shrink the text size
+        m_pdTextSize = std::max(toPixels(m_config.patternMinTextSize),
+                                m_pdTextSize * pdMaxWidth / pdWidth);
+        pdWidth = doFormat(pdFormats[0]);
+    } else {
+        // most compact format fits -> try successively less compact formats
+        // until it stops fitting
+        const PDFormat *format = pdFormats;
+        while (format[1].channelFormat) {
+            pdWidth = doFormat(format[1]);
+            if (pdWidth > pdMaxWidth) { break; }
+            ++format;
+        }
+        // use the least compact format that did fit
+        if (pdWidth > pdMaxWidth) { pdWidth = doFormat(format[0]); }
+    }
+
+    // set up pattern display geometry -- step 4: center
+    int pdXoffset = (m_metaStartX - pdWidth) >> 1;
+    m_pdPosX = pdXoffset;
+    m_pdChannelX0 += pdXoffset;
+    m_pdBarStartX = pdXoffset - toPixels(m_config.patternBarPaddingX);
+    m_pdBarEndX = pdXoffset + pdWidth + toPixels(m_config.patternBarPaddingX);
+
+    // done!
+    Dprintf("updateLayout(): pdTextSize=%d pdRows=%d\n", m_pdTextSize, m_pdRows);
+}
+
+void Application::formatPosition(int order, int pattern, int row, char* text, char* attr, int size) {
+    auto makeNum = [&] (int num, char a, char sep) {
+        if  (size >= 3) { *text++ = char((num / 100) % 10) + '0';  *attr += a;  --size; }
+        if  (size >= 2) { *text++ = char((num /  10) % 10) + '0';  *attr += a;  --size; }
+        if  (size >= 1) { *text++ = char( num        % 10) + '0';  *attr += a;  --size; }
+        if ((size >= 1) && sep) { *text++ = sep;                 *attr += ':';  --size; }
+    };
+    if (size >= 7) { makeNum(order,   'O', ':'); }
+    if (size >= 7) { makeNum(pattern, 'P', '.'); }
+                     makeNum(row,     'R', 0);
+    while (size > 0) { *text++ = ' ';  *attr++ = ' ';  --size; }
+    *text = '\0';
+    *attr = '\0';
 }

src/app.cpp
@@ -36,12 +36,45 @@ void Application::init(int argc, char* argv[]) {
 void Application::draw(float dt) {
     (void)dt;
 
+    // latch current position
+    if (m_mod) {
+        AudioMutexGuard mtx_(m_sys);
+        m_currentOrder = m_mod->get_current_order();
+        int pat = m_mod->get_current_pattern();
+        if (pat != m_currentPattern) { m_patternLength = m_mod->get_pattern_num_rows(pat); }
+        m_currentPattern = pat;
+        m_currentRow = m_mod->get_current_row();
+    }
+
     // set background color
     uint32_t clearColor = m_mod ? m_config.patternBackground : m_config.emptyBackground;
     glClearColor(float( clearColor        & 0xFF) * float(1.f/255.f),
                  float((clearColor >>  8) & 0xFF) * float(1.f/255.f),
                  float((clearColor >> 16) & 0xFF) * float(1.f/255.f), 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // draw pattern display
+    if (m_mod) {
+        m_renderer.box(m_pdBarStartX, m_pdTextY0, m_pdBarEndX, m_pdTextY0 + m_pdTextSize,
+                       m_config.patternBarBackground, m_config.patternBarBackground,
+                       m_pdBarRadius);
+        char posText[16], posAttr[16];
+        for (int dRow = -m_pdRows;  dRow <= m_pdRows;  ++dRow) {
+            int row = dRow + m_currentRow;
+            if ((row < 0) || (row >= m_patternLength)) { continue; }
+            float y = float(m_pdTextY0 + dRow * m_pdTextDY);
+            if (m_pdPosChars) {
+                formatPosition(m_currentOrder, m_currentPattern, row, posText, posAttr, m_pdPosChars);
+                drawPatternDisplayCell(float(m_pdPosX), y, posText, posAttr, false);
+            }
+            for (int ch = 0;  ch < m_numChannels;  ++ch) {
+                drawPatternDisplayCell(float(m_pdChannelX0 + ch * m_pdChannelDX), y,
+                       m_mod->format_pattern_row_channel(m_currentPattern, row, ch, m_pdChannelChars).c_str(),
+                    m_mod->highlight_pattern_row_channel(m_currentPattern, row, ch, m_pdChannelChars).c_str(),
+                    (m_pdPosChars > 0) || (ch > 0));
+            }
+        }
+    }
 
     // draw info box
     if (m_infoEndY > 0) {
@@ -81,6 +114,36 @@ void Application::draw(float dt) {
 
 if (m_mod && m_sys.isPlaying()) { printf("@ %03d.%02X \r", m_mod->get_current_order(), m_mod->get_current_row()); fflush(stdout); }
     m_renderer.flush();
+}
+
+void Application::drawPatternDisplayCell(float x, float y, const char* text, const char* attr, bool pipe) {
+    const float sz = float(m_pdTextSize);
+    if (pipe) {
+        m_renderer.text(x - m_pdPipeDX, y, sz, "|", 0u, m_config.patternSepColor);
+    }
+    char c[2] = " ";
+    while (*text) {
+        c[0] = *text++;
+        char a = *attr;
+        uint32_t color;
+        switch (a) {
+            case '.': color = m_config.patternDotColor;         break;
+            case 'n': color = m_config.patternNoteColor;        break;
+            case 'm': color = m_config.patternSpecialColor;     break;
+            case 'i': color = m_config.patternInstrumentColor;  break;
+            case 'u': color = m_config.patternVolEffectColor;   break;
+            case 'v': color = m_config.patternVolParamColor;    break;
+            case 'e': color = m_config.patternEffectColor;      break;
+            case 'f': color = m_config.patternEffectParamColor; break;
+            case 'O': color = m_config.patternPosOrderColor;    break;
+            case 'P': color = m_config.patternPosPatternColor;  break;
+            case 'R': color = m_config.patternPosRowColor;      break;
+            case ':': color = m_config.patternPosDotColor;      break;
+            default:  color = m_config.patternTextColor;        break;
+        }
+        x = m_renderer.text(x, y, sz, c, 0u, color);
+        if (a) { ++attr; }
+    }
 }
 
 void Application::shutdown() {
@@ -153,6 +216,9 @@ void Application::unloadModule() {
     m_title.clear();
     m_artist.clear();
     m_details.clear();
+    m_numChannels = 0;
+    m_currentPattern = -1;
+    m_patternLength = 0;
     m_sys.setWindowTitle(baseWindowTitle);
     Dprintf("module unloaded, playback paused\n");
 }
@@ -252,6 +318,7 @@ bool Application::loadModule(const char* path) {
 
     // done!
     m_sys.setWindowTitle((m_filename + " - " + baseWindowTitle).c_str());
+    m_numChannels = m_mod->get_num_channels();
     updateLayout();
     return true;
 }

src/app.h
@@ -31,6 +31,13 @@ class Application {
     std::string m_title;
     std::string m_details;
 
+    // current position and size information
+    int m_currentOrder;
+    int m_currentPattern;
+    int m_currentRow;
+    int m_numChannels;
+    int m_patternLength;
+
     // computed layout information (from updateLayout())
     int m_screenSizeX, m_screenSizeY;
     int m_emptyTextSize;
@@ -38,6 +45,12 @@ class Application {
     int m_infoEndY, m_infoShadowEndY;
     int m_infoKeyX, m_infoValueX;
     int m_infoFilenameY, m_infoArtistY, m_infoTitleY, m_infoDetailsY;
+    int m_metaStartX, m_metaShadowStartX;
+    int m_pdPosChars, m_pdChannelChars;
+    int m_pdTextSize, m_pdTextY0, m_pdTextDY, m_pdRows;
+    int m_pdPosX, m_pdChannelX0, m_pdChannelDX;
+    float m_pdPipeDX;
+    int m_pdBarStartX, m_pdBarEndX, m_pdBarRadius;
 
 public:  // interface from SystemInterface
     explicit inline Application(SystemInterface& sys) : m_sys(sys) {}
@@ -58,4 +71,6 @@ private:  // business logic
     int toPixels(int value);
     int textWidth(int size, const char* text);
     void updateLayout();
+    void drawPatternDisplayCell(float x, float y, const char* text, const char* attr, bool pipe=true);
+    static void formatPosition(int order, int pattern, int row, char* text, char* attr, int size);
 };

src/config.h
@@ -70,5 +70,27 @@ struct Config {
     uint32_t infoValueColor           = 0xFFFFFFFFu;
     uint32_t infoDetailsColor         = 0xC0FFFFFFu;
 
+    int      patternTextSize          = 32;
+    int      patternMinTextSize       = 10;
+    int      patternLineSpacing       = 0;
+    int      patternMarginX           = 8;
+    int      patternBarPaddingX       = 4;
+    int      patternBarBorderPercent  = 20;
+    uint32_t patternBarBackground     = 0x40FFFFFFu;
+    uint32_t patternTextColor         = 0xC0FFFFFFu;
+    uint32_t patternDotColor          = 0x40FFFFFFu;
+    uint32_t patternNoteColor         = 0xFFFFFFFFu;
+    uint32_t patternSpecialColor      = patternNoteColor;
+    uint32_t patternInstrumentColor   = makeRGBA(.5f, 1.f, .5f, .875f);
+    uint32_t patternVolEffectColor    = makeRGBA(.5f, .5f, 1.f, 1.f);
+    uint32_t patternVolParamColor     = patternVolEffectColor;
+    uint32_t patternEffectColor       = makeRGBA(1.f, .5f, .5f, .75f);
+    uint32_t patternEffectParamColor  = patternEffectColor;
+    uint32_t patternPosOrderColor     = patternTextColor;
+    uint32_t patternPosPatternColor   = patternTextColor;
+    uint32_t patternPosRowColor       = patternTextColor;
+    uint32_t patternPosDotColor       = 0x80FFFFFFu;
+    uint32_t patternSepColor          = 0x20FFFFFFu;
+
     inline Config() {}
 };

commit 02935531705d1d8ab2546488421aef86b7bf776f

fix MSVC warning / compile error
introduced in 3c73974a0

src/app_layout.cpp
@@ -88,8 +88,8 @@ void Application::updateLayout() {
 
     // set up pattern display geometry -- step 2: the actual formatting function
     auto doFormat = [&] (const PDFormat &fmt) -> int {
-        m_pdPosChars = strlen(fmt.posFormat);
-        m_pdChannelChars = strlen(fmt.channelFormat);
+        m_pdPosChars = int(strlen(fmt.posFormat));
+        m_pdChannelChars = int(strlen(fmt.channelFormat));
         int gapWidth = textWidth(m_pdTextSize, fmt.sep);
         m_pdTextY0 = (m_infoEndY + m_screenSizeY - m_pdTextSize) >> 1;
         m_pdTextDY = m_pdTextSize + toPixels(m_config.patternLineSpacing);