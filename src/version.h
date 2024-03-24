a6f0a73b2092de4d97b9a789ab85a759aa1a7430
Martin Fiedler <keyj@emphy.de>
2024-03-24 22:15:12 +0100

    bump version number to 1.1.0-rc1

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 #pragma once
 constexpr inline const char* g_ProductName    = "TrackMeister";
-constexpr inline const char* g_ProductVersion = "1.0.1-pre";
+constexpr inline const char* g_ProductVersion = "1.1.0-rc1";
